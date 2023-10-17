36e54807f396ce5d47c6c867d8ec0a02b7ef4b23
shanakaGit <134135784+shanakaGit@users.noreply.github.com>
2023-10-17 14:36:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+
+export default function App() {
+
+  useEffect(()=>{
+    fetch('http://fakestoreapi.com/products').
+    then((resp)=> resp.json()).
+    then((json)=> console.log(json))
+  },[])
+
+  return (
+    <div>
+      <h1>gp-api-calling testing</h1>
+    </div>
+  )
+}
